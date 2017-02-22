b18bd0ac79124e87c40b7d857771833d67ab29fd
Jason Smith <jason@smithss.org>
2017-02-22 12:37:36 -0800

    wrapper script to run xhost & start/create container

---
+++ chrome/wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+### This wrapper script sets xhost and starts the chrome docker container.
+
+
+xhost local:root > /dev/null
+
+if [[ $(docker ps -a|grep chrome) ]]; then
+    docker start chrome
+else
+    echo "Creating "Chrome" container"
+   docker run -d --memory 512mb --net host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -v $HOME/Downloads:/home/chrome/Downloads -v $HOME/.config/google-chrome:/data -v /run/dbus/:/run/dbus/:rw -v /dev/shm:/dev/shm --device /dev/snd --name chrome --privileged aslaen/chrome --force-device-scale-factor=1 &>/dev/null
+fi
